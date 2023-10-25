ad836d098f1cdaa94cea0f1845503b7f0e30d17e
mizuhar <128784828+mizuhar@users.noreply.github.com>
2023-10-25 10:12:23 +0300

    Add files via upload

---
+++ Layout-bottom.jsx
@@ -0,0 +1,101 @@
+export default function LayoutBottom(){
+    return(<section className="client_section layout_padding-bottom">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-10 col-lg-8 mx-auto">
+          <div className="client_container">
+            <div className="heading_container">
+              <h2>What Says Our students</h2>
+              <p>
+                iusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis n
+              </p>
+            </div>
+            <div className="client_box-container">
+              <div className="carousel-wrap ">
+                <div className="owl-carousel">
+                  <div className="item">
+                    <div className="box">
+                      <div className="img-box">
+                        <img src="images/client1.png" alt="" />
+                      </div>
+                      <div className="detail-box">
+                        <h4>Jone Moe</h4>
+                        <p>
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum{" "}
+                        </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="box">
+                      <div className="img-box">
+                        <img src="images/client2.png" alt="" />
+                      </div>
+                      <div className="detail-box">
+                        <h4>Lio Uji</h4>
+                        <p>
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum{" "}
+                        </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="box">
+                      <div className="img-box">
+                        <img src="images/client1.png" alt="" />
+                      </div>
+                      <div className="detail-box">
+                        <h4>Jone Moe</h4>
+                        <p>
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum{" "}
+                        </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="box">
+                      <div className="img-box">
+                        <img src="images/client2.png" alt="" />
+                      </div>
+                      <div className="detail-box">
+                        <h4>Lio Uji</h4>
+                        <p>
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum{" "}
+                        </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>)
+}
